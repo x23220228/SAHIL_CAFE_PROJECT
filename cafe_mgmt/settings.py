c14c1762e2fc344f6661d057ba3706ee6d16a586
EC2 Default User <ec2-user@ip-172-31-31-70.eu-west-2.compute.internal>
2024-04-05 00:41:23 +0000

    Commit

--- cafe_mgmt/settings.py
+++ cafe_mgmt/settings.py
@@ -96,7 +96,7 @@ LOCALE_PATHS = [
 ]
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/ec2-user/environment/CAFE_MANAGEMENT/staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'my_cafe/static'),
 ]
